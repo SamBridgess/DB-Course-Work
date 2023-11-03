6e75a4fffa550397bd9b57ccd967ab9b7b124ce9
SamBridgess <ilya.minyaeff@yandex.ru>
2023-11-03 13:28:30 +0300

    added triggers for kd

--- triggers.sql
+++ triggers.sql
@@ -9,8 +9,26 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_winrate_trigger
+CREATE OR REPLACE TRIGGER update_winrate_trigger
 BEFORE INSERT OR UPDATE OF wins, loses
 ON user_stats
 FOR EACH ROW
 EXECUTE FUNCTION calculate_winrate();
+
+
+CREATE OR REPLACE FUNCTION calculate_kd()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Calculate the winrate
+    IF NEW.deaths != 0 THEN
+        NEW.kd_ratio = NEW.kills / NEW.deaths;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER update_kd_trigger
+BEFORE INSERT OR UPDATE OF kills, deaths
+ON user_stats
+FOR EACH ROW
+EXECUTE FUNCTION calculate_kd();
