27cca7a681af51359e11acfd34697863ed7fb3e5
SamBridgess <ilya.minyaeff@yandex.ru>
2023-11-03 13:50:26 +0300

    added triggers for new maps

--- triggers.sql
+++ triggers.sql
@@ -19,7 +19,7 @@ EXECUTE FUNCTION calculate_winrate();
 CREATE OR REPLACE FUNCTION calculate_kd()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- Calculate the winrate
+    -- Calculate the kd
     IF NEW.deaths != 0 THEN
         NEW.kd_ratio = NEW.kills / NEW.deaths;
     END IF;
@@ -32,3 +32,19 @@ BEFORE INSERT OR UPDATE OF kills, deaths
 ON user_stats
 FOR EACH ROW
 EXECUTE FUNCTION calculate_kd();
+
+
+CREATE OR REPLACE FUNCTION add_map()
+RETURNS TRIGGER AS $$
+BEGIN
+  INSERT INTO map_stats (user_id, map_id, rounds_played, win_rate)
+  SELECT id, NEW.id, 0, 0 FROM users;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER add_user_maps
+AFTER INSERT ON maps
+FOR EACH ROW
+EXECUTE FUNCTION add_map();
