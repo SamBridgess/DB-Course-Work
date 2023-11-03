331494bf36b75b53157e3236dc7be40452073078
SamBridgess <ilya.minyaeff@yandex.ru>
2023-11-03 12:03:48 +0300

    more inserts

--- insert.sql
+++ insert.sql
@@ -35,6 +35,12 @@ VALUES
     ('Villa', true, 8),
     ('Yacht', true, 5);
 
+INSERT INTO map_stats(user_id, map_id, rounds_played, win_rate)
+VALUES
+    (1, 1, 100, 1.3),
+    (1, 2, 100, 1.1),
+    (1, 3, 100, 0.9);
+
 INSERT INTO operator(name, speed, armor, team, ability, price, win_rate, rounds_played, difficulty, owner)
 VALUES
     ('Sledge', 2, 2, true, 'Tactical Breaching Hammer', 1000, 1.1, 110, 3, 1),
@@ -58,5 +64,53 @@ VALUES
     ('Tachanka', 2, 2, false, 'Grenade Launcher', 1000, 1.1, 110, 7, 1),
     ('Jager', 2, 2, false, 'Active Defense System', 1000, 1.1, 110, 6, 1),
     ('Bandit', 3, 1, false, 'Shock Wire', 1000, 1.1, 110, 8, 1);
+
+
+INSERT INTO secondary_gadget(name, team)
+VALUES
+    ('Observation Blocker', false),
+    ('C4', false),
+    ('Impact Grenade', false),
+    ('Barbed Wire', false),
+    ('Bulletproof Camera', false),
+    ('Proximity Alarm', false),
+    ('Deployable Shield', false),
+    ('Hard Breach Charge', true),
+    ('Breach Charge', true),
+    ('Frag Grenade', true),
+    ('Claymore', true),
+    ('Stun Grenade', true),
+    ('Smoke Grenade', true),
+    ('Impact EMP Grenade', true);
+
+INSERT INTO operator_gadget(operator_id, gadget_id)
+VALUES
+    (1, 10), (1, 12), (1, 14),
+    (2, 11), (2, 9),
+    (3, 9), (3, 11),
+    (4, 12), (4, 13),
+    (5, 13), (5, 11),
+    (6, 8), (6, 13), (6, 14),
+    (7, 13), (7, 10), (7, 11),
+    (8, 9), (8, 10), (8, 13),
+    (9, 13), (9, 9),
+    (10, 9), (10, 11),
+    (11, 4), (11, 6),
+    (12, 2), (12, 5),
+    (13, 4), (13, 6),
+    (14, 2), (14, 7), (14, 1),
+    (15, 4), (15, 5),
+    (16, 6), (16, 3), (16, 1),
+    (17, 3), (17, 2),
+    (18, 4), (18, 7),
+    (19, 5), (19, 1),
+    (20, 4), (20, 2);
+
+INSERT INTO operator_skin(name, price, operator)
+VALUES
+    ('Six invitational 2019', 20000, 3),
+    ('G2 Esports 2019', 30000, 19),
+    ('Wind Bastion', 15000, 1);
+
 
 
